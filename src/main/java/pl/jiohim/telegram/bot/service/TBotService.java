576c6b37b728d35cd8923c242133c9ffc01f5423
Jiohim <bohdankrasin@gmail.com>
2023-12-21 12:24:55 +0100

    initial commit

---
+++ src/main/java/pl/jiohim/telegram/bot/service/TBotService.java
@@ -0,0 +1,20 @@
+package pl.jiohim.telegram.bot.service;
+
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class TBotService  extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+}
---
+++ src/test/java/pl/jiohim/telegram/bot/JioHimAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jiohim.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JioHimAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
