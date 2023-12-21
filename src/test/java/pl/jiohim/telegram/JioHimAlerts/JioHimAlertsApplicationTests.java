b4e4335e433ace2d95b41077b93a9c1cdbd11d03
Jiohim <bohdankrasin@gmail.com>
2023-12-21 12:24:55 +0100

    initial commit

---
+++ src/test/java/pl/jiohim/telegram/JioHimAlerts/JioHimAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jiohim.telegram.JioHimAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JioHimAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
